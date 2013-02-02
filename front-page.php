4f4516249e80c14ad6556e1735d85871c25ed6c1
Kenrick Turner <kenrickturner@gmail.com>
2013-02-02 15:22:55 +0000

    Moving content of front-page.php into Wordpress
    Rather than having it hardcoded into the template, let's stick it in the Wordpress db so Katharine can update it in the future

--- front-page.php
+++ front-page.php
@@ -7,12 +7,6 @@ get_header(); ?>
 
 		<!-- Content -->
 		<div id="content" class="nine columns centered" role="content">
-			<h2 class="site-title">Katharine Turner</h2>
-			<div class="row contents-titles">
-				<div class="four columns site-title"><h4><a href="hello" title="Say hello">Hello</a></h3></div>
-				<div class="four columns site-title"><h4><a href="portfolio" title="View my portfolio">Portfolio</a></h3></div>
-				<div class="four columns site-title"><h4><a href="blog" title="Read my blog">Blog</a></h3></div>
-			</div>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php the_content(); ?>
